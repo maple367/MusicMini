1dc7a12be6927f2654e7a148a49927b17d3e52b6
maple367 <thesea2000@outlook.com>
2023-11-26 00:41:01 +0800

    modified:   README.md 	modified:   dist/js/danMu.js

--- dist/js/danMu.js
+++ dist/js/danMu.js
@@ -36,47 +36,48 @@ setInterval(function () {
                                 //切换歌词
                                 TopMsg("歌词切换", "", false);
                                 SpectrumSwichChange();
+                            } else if (adminMsg[i].text.slice(0, 4) == "#点歌 ") {
+                                //点歌函数
+                                GetOrederList(adminMsg[i].text.slice(4))
+                            } else if (adminMsg[i].text.slice(0, 3) == "#切歌") {
+                                //切歌
+                                TopMsg("已切歌", "房管", false);
+                                NextMusic(true, true);
                             }
                         }
                     }
                 }
-                //点歌切歌
+                //游客功能:点歌，切歌
                 if (DanmuOrderSwich == true) {
                     for (i in roomMsg) {
                         if (((Date.parse(new Date()) / 1000) - 20) <= timeLine(roomMsg[i].timeline)) {
-                            if (roomMsg[i].text.slice(0, 4) == "#点歌 ") {
-                                //相关函数
-                                if (Number(roomMsg[i].isadmin) == 1) {
-                                    //管理员点歌
-                                    //点歌函数
-                                    GetOrederList(roomMsg[i].text.slice(4))
-                                } else if (order_CD_list.includes(roomMsg[i].uid)) {
-                                    //点歌CD
-                                    TopMsg("点歌失败", "冷却中", true);
-                                } else if (order_list.length >= orderListLength) {
-                                    //歌单已满
-                                    TopMsg("点歌失败", "歌单已满", true);
-                                } else {
-                                    //观众点歌
-                                    order_CD_list.push(roomMsg[i].uid);
-                                    //点歌函数
-                                    GetOrederList(roomMsg[i].text.slice(4))
+                            if (Number(roomMsg[i].isadmin) != 1) {
+                                if (roomMsg[i].text.slice(0, 4) == "#点歌 ") {
+                                    //相关函数
+                                    if (order_CD_list.includes(roomMsg[i].uid)) {
+                                        //点歌CD
+                                        TopMsg("点歌失败", "冷却中", true);
+                                    } else if (order_list.length >= orderListLength) {
+                                        //歌单已满
+                                        TopMsg("点歌失败", "歌单已满", true);
+                                    } else {
+                                        //观众点歌
+                                        order_CD_list.push(roomMsg[i].uid);
+                                        //点歌函数
+                                        GetOrederList(roomMsg[i].text.slice(4))
+                                    }
                                 }
-                            }
-                            else if (roomMsg[i].text.slice(0, 3) == "#切歌") {
-                                //切歌
-                                if (next_CD_list.includes(roomMsg[i].uid)) {
-                                    //切歌CD
-                                    TopMsg("切歌失败", "冷却中", true);
-                                } else if (Number(roomMsg[i].isadmin) == 1) {
-                                    //管理员消息切歌
-                                    TopMsg("已切歌", "房管", false);
-                                    NextMusic(true, true);
-                                } else {
-                                    //观众切歌
-                                    next_CD_list.push(roomMsg[i].uid);
-                                    TopMsg("已切歌", "观众", false);
-                                    NextMusic(true, true);
+                                else if (roomMsg[i].text.slice(0, 3) == "#切歌") {
+                                    //切歌
+                                    if (next_CD_list.includes(roomMsg[i].uid)) {
+                                        //切歌CD
+                                        TopMsg("切歌失败", "冷却中", true);
+                                    } else {
+                                        //观众切歌
+                                        next_CD_list.push(roomMsg[i].uid);
+                                        TopMsg("已切歌", "观众", false);
+                                        NextMusic(true, true);
+                                    }
                                 }
                             }
                         }
